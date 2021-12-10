fd30854696d552f22d84be102a27a3db2633c6ac
Chloe ASSASSIN <cassassi@e3r3p6.42.fr>
2021-12-10 17:33:58 +0100

    premier essais peu concluants

--- philo.c
+++ philo.c
@@ -12,27 +12,29 @@
 
 #include "philo.h"
 
-int ft_error()
+int ft_error(int err)
 {
-	printf("Error\n");
+	printf("Error %d\n", err);
 	exit(EXIT_FAILURE);
 }
 
 void *fonction(void *params)
 {
-	t_philo *here;
+	t_philo here;
 
-	here = (t_philo *)params;
-	if (pthread_mutex_lock(&here->fork) != 0)
-		ft_error();
-	printf("%d Philo %d is thinking and letting himself starve to death, sorry \n", ft_timer(&here->time), here->name);
-	usleep(500);
-	if (pthread_mutex_unlock(&here->fork) != 0)
-		ft_error();
+	here = *(t_philo *)params;
+	if (pthread_mutex_lock(&here.fork) != 0)
+		ft_error(5);
+	printf("%d Philo %d is thinking\n", ft_timer(&here.time), here.name);
+	sleep(1);
+	printf("%d starve to death at %d\n", here.name, ft_timer(&here.time));
+	usleep(600);
+	if (pthread_mutex_unlock(&here.fork) != 0)
+		ft_error(6);
 	return (0);
 }
 
-void ft_create_philo(int philosophers, t_philo *params)
+/*void ft_create_philo(int philosophers, t_philo *params)
 {
 	pthread_t philo[philosophers];
 	int		i;
@@ -47,37 +49,67 @@ void ft_create_philo(int philosophers, t_philo *params)
 	}
 	i = 0;
 	while (i < philosophers)
+
 	{
 		if (pthread_join(philo[i], NULL) != 0)
 			ft_error();
 		i++;
 	}
-}
+}*/
 
 int main(int ac, char **av)
 {
 	int philosophers;
 	int forks;
-	t_philo params;
+	pthread_t philo[philosophers];
+	int		i;
+	t_philo *params[philosophers];
+	t_data plop;
 
-  	gettimeofday(&params.time.start, NULL);
+  	gettimeofday(&plop.start, NULL);
 	if (ac != 5 && ac != 6) 
 	{
 		printf("Error : Wrong number of argument\n");
 		return (1);
 	}
+	i = 0;
 	philosophers = ft_atoi(av[1]);
-	params.die = ft_atoi(av[2]);
-	params.eat = ft_atoi(av[3]);
-	params.sleep = ft_atoi(av[4]);
-	if (ac == 6)
-		params.meal = ft_atoi(av[5]);
-	else
-		params.meal = -2;
-	forks = philosophers;
-	pthread_mutex_init(&params.fork, NULL);
-	ft_create_philo(philosophers, &params);
-	if (pthread_mutex_destroy(&params.fork) != 0)
-		ft_error();
+
+	while (i < philosophers)
+	{
+		params[i] = malloc(sizeof(t_philo));
+	if (!params[i])
+		ft_error(3);
+		params[i]->die = ft_atoi(av[2]);
+		params[i]->eat = ft_atoi(av[3]);
+		params[i]->sleep = ft_atoi(av[4]);
+		if (ac == 6)
+			params[i]->meal = ft_atoi(av[5]);
+		else
+			params[i]->meal = -2;
+		params[i]->name = i + 1;
+		params[i]->time.start = plop.start;
+		pthread_mutex_init(&params[i]->fork, NULL);
+		if (pthread_create(&philo[i], NULL, &fonction, &params[i]) != 0)
+			ft_error(0);
+		i++;
+	}
+	i = 0;
+	while (i < philosophers)
+	{
+		if (pthread_join(philo[i], NULL) != 0)
+			ft_error(1);
+		i++;
+	}
+//	ft_create_philo(philosophers, &params);
+	i = 0;
+	while (i < philosophers)
+	{
+		if (pthread_mutex_destroy(&params[i]->fork) != 0)
+			ft_error(4);
+		free(params[i]);
+		i++;
+	}
+
 	return (0);
 }
